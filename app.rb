d7ed98c036b899530ff70445c58c973e88d52012
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-05-29 20:26:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jean-Kenel"
+  end
+
+  get '/hometown' do
+    "My hometown is Port-au-Prince"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is New Wine"
+  end
+
 end
