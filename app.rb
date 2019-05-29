b91187b8b5dff6fcb16d0a409c6f5bbcf3f5607b
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-05-29 20:25:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-
+    "My name is Jean-Kenel"
   end
 
   get '/hometown' do
-
+    "My hometown is Port-au-Prince"
   end
 
   get '/favorite-song' do
